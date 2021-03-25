5cbda062c2bc907853a5672afc6db6f310fcacb8
HYPech <TCTORBAO@GMAIL.COM>
2021-03-25 10:52:54 -0400

    Update MainActivity.java

--- app/src/main/java/com/hypech/case83_room/MainActivity.java
+++ app/src/main/java/com/hypech/case83_room/MainActivity.java
@@ -2,7 +2,6 @@ package com.hypech.case83_room;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
